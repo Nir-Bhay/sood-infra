18de4f3e0ec49edc2bdfb88402785a82f62a4023
Nir-Bhay <nirbhayhiwse37@gmail.com>
2024-12-02 17:30:17 +0530

    changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,105 +15,42 @@
 
 const pastProjects = [
     {
-        title: "Completed Residential Project",
+        title: "Luxury Residential Township",
         imgUrl: "/assets/images/blog/20240703_110250.jpg",
-        description: "A modern residential project completed with a focus on aesthetics and functionality. This project features spacious interiors, eco-friendly materials, and a design that harmonizes with the natural environment."
+        description: "An expansive residential township featuring eco-friendly materials, modern design, and an emphasis on comfort. The project showcases sustainable living with lush green spaces and community amenities."
     },
     {
-        title: "Commercial Office Bu/ilding",
+        title: "Corporate Tower Construction",
         imgUrl: "assets/images/blog/Brown And Orange Modern Home Building Services Instagram Post (1).png",
-        description: "High-tech commercial spaces with sustainable architecture, offering state-of-the-art facilities. Designed to foster productivity and collaboration among businesses, this building incorporates green technologies."
+        description: "A state-of-the-art corporate building with energy-efficient designs and premium office spaces. Tailored for businesses seeking innovative, collaborative environments."
     },
     {
-        title: "Green Energy Housing",
+        title: "Eco-Conscious Housing Project",
         imgUrl: "/assets/images/Residential Project.jpeg",
-        description: "Eco-friendly housing utilizing solar energy and sustainable materials. This community aims to minimize environmental impact while providing comfortable living spaces."
+        description: "Residential units that combine aesthetic appeal with sustainability. The project integrates renewable energy sources and green building practices for a modern yet eco-friendly lifestyle."
     }
-    // ,
-    // {
-    //     title: "Urban Revival Apartments",
-    //     imgUrl: "images/Commercial Project.jpeg",
-    //     description: "An apartment complex reviving urban living with modern amenities. The design focuses on community engagement and access to local resources."
-    // },
-    // {
-    //     title: "Luxury Villas Project",
-    //     imgUrl: "images/Residential Project.jpeg",
-    //     description: "High-end villas designed with luxury and comfort in mind. Each villa features private gardens, advanced home automation, and luxurious finishes to create an opulent living experience."
-    // },
-    // {
-    //     title: "Historical Building Restoration",
-    //     imgUrl: "images/Commercial Project.jpeg",
-    //     description: "Restoration of a historical building with a modern touch. This project emphasizes preserving the architectural heritage while integrating modern functionalities."
-    // }
 ];
 
 const ongoingProjects = [
     {
-        title: " Shri Naveen Chambers",
+        title: "Shri Naveen Chambers",
         imgUrl: "/assets/images/projects/Sood Infra , construction , Soodinfra office.webp",
-        description: "Located on the Nagpur-Chhindwara Highway, ensuring high visibility and easy accessibility."
+        description: "Located on the Nagpur-Chhindwara Highway, Shri Naveen Chambers is a landmark commercial property with excellent connectivity and strategic placement for maximum business visibility."
     },
     {
-        title: "Commercial Office Building",
+        title: "Commercial Business Park",
         imgUrl: "/assets/images/SingPost Centre to turn into Singapore's first retail mall to offer online shopping.jpeg",
-        description: "High-tech commercial spaces with sustainable architecture, offering state-of-the-art facilities. Designed to foster productivity and collaboration among businesses, this building incorporates green technologies."
+        description: "This ongoing project features cutting-edge commercial office spaces with a focus on sustainable development. The business park is designed to foster innovation and collaboration in a dynamic work environment."
     },
     {
-        title: "Green Energy Housing",
+        title: " Habitat Community",
         imgUrl: "/assets/images/Residential Project.jpeg",
-        description: "Eco-friendly housing utilizing solar energy and sustainable materials. This community aims to minimize environmental impact while providing comfortable living spaces."
+        description: "A modern community project emphasizing green energy solutions, solar power integration, and sustainable living. This housing initiative offers a perfect blend of luxury and eco-consciousness."
     }
-    // ,
-    // {
-    //     title: "Tech Hub Offices",
-    //     imgUrl: "images/Commercial Project.jpeg",
-    //     description: "High-tech office spaces aimed at innovation and collaboration, equipped with the latest technology to support creative work environments."
-    // },
-    // {
-    //     title: "Retail & Residential Hybrid",
-    //     imgUrl: "images/Mixed-Use Development.jpeg",
-    //     description: "A mixed-use building with retail spaces and residential apartments, fostering a convenient lifestyle that promotes local shopping."
-    // },
-    // {
-    //     title: "Downtown Plaza Expansion",
-    //     imgUrl: "images/Luxury Apartment Complex.jpeg",
-    //     description: "Expanding an urban plaza with additional amenities and green space, enhancing the city's public gathering areas and improving community life."
-    // }
 ];
 
-const futureProjects = [
-    {
-        title: "Completed Residential Project",
-        imgUrl: "/assets/images/Residential Project.jpeg",
-        description: "A modern residential project completed with a focus on aesthetics and functionality. This project features spacious interiors, eco-friendly materials, and a design that harmonizes with the natural environment."
-    },
-    {
-        title: "Commercial Office Building",
-        imgUrl: "/assets/images/SingPost Centre to turn into Singapore's first retail mall to offer online shopping.jpeg",
-        description: "High-tech commercial spaces with sustainable architecture, offering state-of-the-art facilities. Designed to foster productivity and collaboration among businesses, this building incorporates green technologies."
-    },
-    {
-        title: "Green Energy Housing",
-        imgUrl: "/assets/images/Residential Project.jpeg",
-        description: "Eco-friendly housing utilizing solar energy and sustainable materials. This community aims to minimize environmental impact while providing comfortable living spaces."
-    }
-    ,
-    {
-        title: "Completed Residential Project",
-        imgUrl: "/assets/images/Residential Project.jpeg",
-        description: "A modern residential project completed with a focus on aesthetics and functionality. This project features spacious interiors, eco-friendly materials, and a design that harmonizes with the natural environment."
-    },
-    {
-        title: "Commercial Office Building",
-        imgUrl: "/assets/images/SingPost Centre to turn into Singapore's first retail mall to offer online shopping.jpeg",
-        description: "High-tech commercial spaces with sustainable architecture, offering state-of-the-art facilities. Designed to foster productivity and collaboration among businesses, this building incorporates green technologies."
-    },
-    {
-        title: "Green Energy Housing",
-        imgUrl: "/assets/images/Residential Project.jpeg",
-        description: "Eco-friendly housing utilizing solar energy and sustainable materials. This community aims to minimize environmental impact while providing comfortable living spaces."
-    }
-];
+
+
 
 const renderProjects = (projects, containerId) => {
     const gallery = document.getElementById(containerId);
